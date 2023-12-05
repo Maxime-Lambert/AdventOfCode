722e93298d0c2ea7f6ce9f05de9d37b630333036
Lambert Maxime <lambert.maxime@protonmail.com>
2023-12-05 10:40:50 +0100

    Solution de la partie 2 du jour 4

--- AdventOfCode/Program.cs
+++ AdventOfCode/Program.cs
@@ -16,4 +16,5 @@ Console.WriteLine("Part 1 sum of engine parts : " + EngineChecker.SolveEngineGam
 Console.WriteLine("Part 2 sum of valid gears : " + EngineChecker.SolveEngineGamePart2("2023/Day3/input.txt"));
 
 Console.WriteLine("Advent of Code - Day 4");
-Console.WriteLine("Part 1 sum of scratchcards points : " + ScratchcardAnalyzer.SolveScratchcard("2023/Day4/input.txt"));
+Console.WriteLine("Part 1 sum of scratchcards points : " + ScratchcardAnalyzer.SolveScratchcardsPart1("2023/Day4/input.txt"));
+Console.WriteLine("Part 2 sum of scratchcards copies : " + ScratchcardAnalyzer.SolveScratchcardsPart2("2023/Day4/input.txt"));
